058b5c64edd7809ac7d1ba9df785b9c13ae163e0
Tyler Chia <tylerchia7@gmail.com>
2020-09-07 00:26:31 -0700

    lab 05 first commit

--- R/labexample.R
+++ R/labexample.R
@@ -103,3 +103,110 @@ ggplot() +
   labs(fill = "")
 }
 nearest_state_plot("Alabama")
+
+
+library(leaflet)
+leaflet() %>%
+  addTiles() %>%
+  addMarkers(lng=-119.8489, lat=34.4140,popup = "UCSB")
+
+leaflet() %>%
+  setView(lng=-119.8489, lat=34.4140,zoom = 12) %>%
+  addProviderTiles(providers$CartoDB)
+
+leaflet() %>%
+  setView(lng=-119.8489, lat=34.4140,zoom = 12) %>%
+  addProviderTiles(providers$Esri.WorldImagery) %>%
+  addProviderTiles(providers$Stamen.TonerLines, options = providerTileOptions(opacity = .5))
+
+leaflet(data = ) %>%
+  addProviderTiles(providers$CartoDB) %>%
+  addCircleMarkers(label = -address, popup = pop,
+                   clusterOptions = markerClusterOptions())
+
+pal <- colorFactor(c("darkgreen", "navy"), domain = c("Goleta","Santa Barbara"))
+
+leaflet(data = ) %>%
+  addProviderTiles(providers$CartoDB) %>%
+  addCircleMarkers(color = -pal(city), fillOpacity = .5, stroke = FALSE) %>%
+  addPolylines(data = , fillColor = "transparent", color = "black")
+
+## Week 5
+library(tidyverse)
+library(sf)
+library(raster)
+library(getlandsat)
+library(mapview)
+library(osmdata )
+
+bb = read_csv("data/uscities.csv") %>%
+  filter(city  == "Palo") %>%
+  st_as_sf(coords = c("lng", "lat"), crs = 4326) %>%
+  st_transform(5070) %>%
+  st_buffer(5000) %>%
+  st_bbox() %>%
+  st_as_sfc
+
+mapview(bb)
+
+#####
+
+bwgs = st_transform(bb, 4326)
+
+osm = osmdata::opq(bwgs) %>%
+  osmdata::add_osm_feature("building") %>%
+  osmdata::osmdata_sf()
+
+mapview(osm$osm_polygons)
+
+#####
+
+bbwgs = st_bbox(bwgs)
+scenes = lsat_scenes()
+
+down = scenes %>%
+  filter(min_lat <= bbwgs$ymin, max_lat >= bbwgs$ymax,
+         min_lon <= bbwgs$xmin, max_lon >= bbwgs$xmax,
+         as.Date(acquisitionDate) == as.Date("2016-09-26"))
+
+write.csv(down, file = "data/palo-flood.csv", row.names = F)
+
+## IN RMD ------------------------------------------------------------
+
+meta = read_csv("data/palo-flood.csv")
+
+files = lsat_scene_files(meta$download_url) %>%
+  filter(grepl(paste0("B",1:6,".TIF$", collapse = "|"), file)) %>%
+  arrange(file) %>%
+  pull(file)
+
+st = sapply(files, lsat_image)
+
+s = stack(st) %>%
+  setNames(paste0("band", 1:6))
+
+cropper = bb %>%
+  st_as_sf() %>%
+  st_transform(crs(s))
+
+r = crop(s, cropper)
+
+par(mfrow = c(1,2))
+
+plotRGB(r, r = 4, g = 3, b = 2)
+
+plotRGB(r, r = 5, g = 4, b = 3, stretch = "hist")
+
+ndvi = (r$band5 - r$band4) / (r$band5 + r$band4)
+
+palette = colorRampPalette(c("blue","white","red"))
+
+plot(ndvi, col = palette(256))
+
+thresholding = function(x){ifelse(x <= 0,1, NA)}
+
+flood = calc(ndvi, thresholding)
+plot(flood)
+
+mapview(flood)
+
